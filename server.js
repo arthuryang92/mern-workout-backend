0a1b72d5ff709978f72a6ed18b13821e6ac4d111
Arthur <arthuryangzeng@hotmail.com>
2023-04-16 00:34:15 +0800

    add cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
+const cors = require("cors");
 require("dotenv").config();
 
 //express app
@@ -9,7 +10,7 @@ const app = express();
 
 //middleware
 app.use(express.json());
-
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
